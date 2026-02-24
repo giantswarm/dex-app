f94beacf835f6022a383e340d073591ca244a23f
Spyros Synodinos <spyros@giantswarm.io>
2026-02-24 18:27:03 +0200

    Refactor upstream image subchart configuration to support registry prefix

--- Makefile.custom.mk
+++ Makefile.custom.mk
@@ -3,5 +3,5 @@ APPLICATION ?= helm/dex-app
 .PHONY: sync-image-tag
 sync-image-tag: ## Sync dex.image.tag in values.yaml from Chart.yaml appVersion
 	@app_version=$$(yq '.appVersion' $(APPLICATION)/Chart.yaml) && \
-	yq -i ".dex.dex.image.tag = \"$$app_version\"" $(APPLICATION)/values.yaml && \
+	yq -i ".dex.image.tag = \"$$app_version\"" $(APPLICATION)/values.yaml && \
 	echo "Synced dex.image.tag to $$app_version"
